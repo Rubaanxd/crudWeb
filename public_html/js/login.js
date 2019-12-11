db25f245147b62f4c4d29d7e9b5967ccb0eb0afd
Rubaanxd <rubaanxd@github.com>
2019-12-10 23:11:24 -0600

    Se agrega y centra Form y se agrega primera prueba de JS

--- public_html/js/login.js
+++ public_html/js/login.js
@@ -1,8 +1,18 @@
 /* 
  * archivo para validcion de Login
  */
-var username = index.html.
-function obtener valores(){
-    
+var username;
+var pass;
+function obtenerValores(){
+    if(document.getElementById("username").value !== null){
+        username = document.getElementById("username").value;
+    }else{
+        alert("El campo de Usuario es Obligatorio");
+    }
+    if(document.getElementById("password").value !== null){
+        pass = document.getElementById("password").value;
+    }else{
+        alert("La contraseña esta vacia");
+    }
 }
 
